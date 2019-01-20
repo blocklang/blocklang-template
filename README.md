db713e749942fa680577d4856d2c44bb7461a6ce
xiaohulu <idocument@qq.com>
2019-01-20 18:14:11 +0800

    初始化 spring boot 项目

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Block Lang Template
 
 项目模板，前端项目模板是一个 Dojo 项目，后端项目模板是一个 Spring Boot 项目。
+
+## 安装
+
+在部署的服务器上安装最新版的 [Maven](http://maven.apache.org/download.cgi)，因为需要使用 Maven 管理依赖。
---
+++ server/src/test/java/com/blocklang/app/AppApplicationTests.java
@@ -0,0 +1,17 @@
+package com.blocklang.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+

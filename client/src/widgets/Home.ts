a2248471d84a2e9987c4b2864eab1b473322d4f1
xiaohulu <idocument@qq.com>
2019-04-19 21:44:03 +0800

    测试自动排版功能

--- client/src/widgets/Home.ts
+++ client/src/widgets/Home.ts
@@ -5,10 +5,10 @@ import * as css from './styles/Home.m.css';
 
 export default class Home extends WidgetBase {
 	protected render() {
-		return v('div', { classes: [css.root, "d-flex justify-content-center align-items-center"] }, [
-			v('h1', {classes: ['shadow-lg p-5 rounded w-100 text-center bg-warning text-white']}, [
+		return v('div', { classes: [css.root, 'd-flex justify-content-center align-items-center'] }, [
+			v('h1', { classes: ['shadow-lg p-5 rounded w-100 text-center bg-warning text-white'] }, [
 				`Hello World`,
-				v('small', {classes:['badge badge-pill badge-light ml-4 text-muted']}, ['v0.1.0'])
+				v('small', { classes: ['badge badge-pill badge-light ml-4 text-muted'] }, ['v0.1.0'])
 			])
 		]);
 	}
